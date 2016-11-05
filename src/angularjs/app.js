4ea387abc908ba9842990005e4dc7b2d0d3e3d03
Esteban <estebandinamarca@gmail.com>
2016-11-05 02:53:56 -0300

    Adding Contentful

--- src/angularjs/app.js
+++ src/angularjs/app.js
@@ -1,49 +1,105 @@
-//'use strict';
+'use strict';
 var app = angular.module('app', ['ngMap']);
 
 app.controller('antesmap', function($scope, $http, $timeout, NgMap) {
   
+  // -----------------------------------------
+  // INIT NG-MAP
+  // -----------------------------------------
   NgMap.getMap().then(function(evtMap) {
     $scope.map = evtMap;
   });
-
   $scope.googleMapsUrl = 'https://maps.googleapis.com/maps/api/js?key=AIzaSyAvc8f2wLp2oBDCFPawvFuoJuIfjo6LYKY';
-  $scope.pauseLoading = true;
 
+  // -----------------------------------------
   // TIMEOUT 2 SECONDS INITIAL LOADING
+  // -----------------------------------------
+  $scope.pauseLoading = true;
   $timeout(function() {
     $scope.pauseLoading = false;
   }, 2000);
 
+  // CENTER MAP
   $scope.cities = [{
-      "id":"1",
-      "name": "Santiago RM",
-      "position":[-33.4378305,-70.65044920000003]
+    "id":"1",
+    "name": "Santiago RM",
+    "position":[-33.4378305,-70.65044920000003]
   }];
 
+  // -----------------------------------------
+  // GET JSON PLACES
+  // -----------------------------------------
   $scope.places = [];
   $scope.selectedCity = [];
-  function getPlaces(places) { 
-    $http.get(places).then(function (response) {
+  function getPlaces() {
+    $scope.jsonUrl = "https://cdn.contentful.com/spaces/"+ $scope.spaceId +"/"+ $scope.infoType +"?"+ $scope.limitTo +"&access_token="+ $scope.accessToken;
+    $http.get($scope.jsonUrl).then(function (response) {
+
+      // ADD ICON INFO AND URL
+      angular.forEach(response.data.items, function(item) {
+        angular.forEach(response.data.includes.Asset, function(asset) {
+          if ( item.fields.icon.sys.id == asset.sys.id ) {
+            item.fields.icon.data = {};
+            item.fields.icon.data = asset.fields;
+          }
+        });
+      });
+
       $scope.places = response.data;
       $scope.selectedCity = $scope.cities[0];
+      //console.log('response.data :' + JSON.stringify(response.data));
     });
   }
-  getPlaces('../src/json/places.json');
+
+  // -----------------------------------------
+  // INFO TYPE
+  // -----------------------------------------
+  $scope.infoType = ['entries'];
+  $scope.addInfoType = function(infoType) {
+    $scope.infoType = [];
+    $scope.infoType.push(infoType);
+  };
+
+  // -----------------------------------------
+  // LIMIT TO
+  // -----------------------------------------
+  $scope.limitTo = ['limit=10'];
+  $scope.addLimitTo = function(limitTo) {
+    $scope.limitTo = [];
+    $scope.limitTo.push(limitTo);
+  };
+
+  // -----------------------------------------
+  // SPACE ID & ACCESS TOKEN
+  // -----------------------------------------
+  $scope.spaceId = ['exxl6su6yxqc'];
+  $scope.accessToken = ['38f9d13b1d29e3fce9d65ec6ccd3bb5f13e88f14e88c3e47a162bee0ea170266'];
+
+  // -----------------------------------------
+  // INIT JSON PLACES
+  // -----------------------------------------
+  getPlaces();
 
 
+  // =========================================
+  // ============== ACTIONS ==================
+  // =========================================
+
+  // -----------------------------------------
+  // OPEN/CLOSE INFO-WINDOWS - LOAD INFO PLACE
+  // -----------------------------------------
   $scope.place = '';
   $scope.showDetail = function(e, place) {
     $scope.place = place;
-    $scope.map.showInfoWindow('foo-iw', place.id);
+    $scope.map.showInfoWindow('iw_id', place.sys.id);
   };
-
   $scope.hideDetail = function() {
-    map.hideInfoWindow('foo-iw');
+    $scope.map.hideInfoWindow('iw_id');
   };
 
-
-  // ABRIR OFF CANVAS - CARGAR INFO PLACE
+  // --------------------------------------------
+  // TOGGLE OFF-CANVAS & OVERLAY CSS
+  // --------------------------------------------
   $scope.openClose = '';
   $scope.overlay = '';
   $scope.toggleCanvas = function(){
