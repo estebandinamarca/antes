2016d42463b7dbaad72eaf0f5a7c9933d50a5efa
Esteban <estebandinamarca@gmail.com>
2017-02-25 23:03:25 -0300

    htaccess

--- src/angularjs/app.js
+++ src/angularjs/app.js
@@ -26,8 +26,8 @@ app.config(function($routeProvider, $locationProvider) {
       });
     $locationProvider
       .html5Mode({
-        enabled: true
-        //requireBase: false,
+        enabled: true,
+        requireBase: true
       })
       .hashPrefix('!');
 });
