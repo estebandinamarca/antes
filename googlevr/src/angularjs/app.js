7a161eac9e3e8dec58dfab350f7c5eec8cf66532
Esteban <estebandinamarca@gmail.com>
2017-03-22 16:33:08 -0300

    VR ANGULAR Y PHOTOSWIPE

--- googlevr/src/angularjs/app.js
+++ googlevr/src/angularjs/app.js
@@ -1,6 +1,14 @@
 'use strict';
 var app = angular.module('app', ['ngRoute', 'ngAnimate', 'oc.lazyLoad']);
 
+
+app.filter('mathround', function() {
+  return function(input) {
+    return Math.ceil(input);
+  };
+});
+
+
 app.config(function($routeProvider, $locationProvider, $controllerProvider) {
   $routeProvider
     .when('/', {
@@ -172,7 +180,7 @@ app.controller('placeController', function($scope, $http, $location, $timeout) {
   $scope.infoType = ['entries'];
 
   // ---------------------------------------------------
-  // --
+  // GET PLACE INFO
   // ---------------------------------------------------
   $scope.pageClass = 'place';
   $scope.placeId = '';
@@ -190,25 +198,38 @@ app.controller('placeController', function($scope, $http, $location, $timeout) {
 
       console.log('$scope.jsonUrl :' + $scope.jsonUrl);
 
-      // ADD ICON INFO AND URL
+      // IMAGENES
       angular.forEach(response.data.items, function(item) {
         angular.forEach(response.data.includes.Asset, function(asset) {
+
+          // filtro icon
           if ( item.fields.icon.sys.id == asset.sys.id ) {
             item.fields.icon.data = {};
             item.fields.icon.data = asset.fields;
           }
+
+          // filtro cover
           if ( item.fields.cover.sys.id == asset.sys.id ) {
             item.fields.cover.data = {};
             item.fields.cover.data = asset.fields;
           }
+
+          // filtro gallery
+          angular.forEach(item.fields.gallery, function(gal) {
+            if ( gal.sys.id == asset.sys.id ) {
+              gal.data = {};
+              gal.data = asset.fields;
+            }
+          });
+
         });
       });
+
       $scope.place = response.data;
-      //kolkokconsole.log(JSON.stringify($scope.place));
-      //$scope.name = 'taj-mahal';
+      console.log(JSON.stringify($scope.place));
 
       // GOOGLE VR VIEW
-      //var name = 'taj-mahal';
+      //var img = 'taj-mahal';
       var img = response.data.items[0].fields.cover.data.file.url;
       function onLoad() {
       var vrView = new VRView.Player('#vrview', {
@@ -225,6 +246,215 @@ app.controller('placeController', function($scope, $http, $location, $timeout) {
   }
 
   getPlace();
+
+
+  // ---------------------------------------------------
+  // GALERIA
+  // ---------------------------------------------------
+  var initPhotoSwipeFromDOM = function(gallerySelector) {
+
+      // parse slide data (url, title, size ...) from DOM elements 
+      // (children of gallerySelector)
+      var parseThumbnailElements = function(el) {
+          var thumbElements = el.childNodes,
+              numNodes = thumbElements.length,
+              items = [],
+              figureEl,
+              linkEl,
+              size,
+              item;
+
+          for(var i = 0; i < numNodes; i++) {
+
+              figureEl = thumbElements[i]; // <figure> element
+
+              // include only element nodes 
+              if(figureEl.nodeType !== 1) {
+                  continue;
+              }
+
+              linkEl = figureEl.children[0]; // <a> element
+
+              size = linkEl.getAttribute('data-size').split('x');
+
+              // create slide object
+              item = {
+                  src: linkEl.getAttribute('href'),
+                  w: parseInt(size[0], 10),
+                  h: parseInt(size[1], 10)
+              };
+
+
+
+              if(figureEl.children.length > 1) {
+                  // <figcaption> content
+                  item.title = figureEl.children[1].innerHTML; 
+              }
+
+              if(linkEl.children.length > 0) {
+                  // <img> thumbnail element, retrieving thumbnail url
+                  item.msrc = linkEl.children[0].getAttribute('src');
+              } 
+
+              item.el = figureEl; // save link to element for getThumbBoundsFn
+              items.push(item);
+          }
+
+          return items;
+      };
+
+      // find nearest parent element
+      var closest = function closest(el, fn) {
+          return el && ( fn(el) ? el : closest(el.parentNode, fn) );
+      };
+
+      // triggers when user clicks on thumbnail
+      var onThumbnailsClick = function(e) {
+          e = e || window.event;
+          e.preventDefault ? e.preventDefault() : e.returnValue = false;
+
+          var eTarget = e.target || e.srcElement;
+
+          // find root element of slide
+          var clickedListItem = closest(eTarget, function(el) {
+              return (el.tagName && el.tagName.toUpperCase() === 'FIGURE');
+          });
+
+          if(!clickedListItem) {
+              return;
+          }
+
+          // find index of clicked item by looping through all child nodes
+          // alternatively, you may define index via data- attribute
+          var clickedGallery = clickedListItem.parentNode,
+              childNodes = clickedListItem.parentNode.childNodes,
+              numChildNodes = childNodes.length,
+              nodeIndex = 0,
+              index;
+
+          for (var i = 0; i < numChildNodes; i++) {
+              if(childNodes[i].nodeType !== 1) { 
+                  continue; 
+              }
+
+              if(childNodes[i] === clickedListItem) {
+                  index = nodeIndex;
+                  break;
+              }
+              nodeIndex++;
+          }
+
+
+
+          if(index >= 0) {
+              // open PhotoSwipe if valid index found
+              openPhotoSwipe( index, clickedGallery );
+          }
+          return false;
+      };
+
+      // parse picture index and gallery index from URL (#&pid=1&gid=2)
+      var photoswipeParseHash = function() {
+          var hash = window.location.hash.substring(1),
+          params = {};
+
+          if(hash.length < 5) {
+              return params;
+          }
+
+          var vars = hash.split('&');
+          for (var i = 0; i < vars.length; i++) {
+              if(!vars[i]) {
+                  continue;
+              }
+              var pair = vars[i].split('=');  
+              if(pair.length < 2) {
+                  continue;
+              }           
+              params[pair[0]] = pair[1];
+          }
+
+          if(params.gid) {
+              params.gid = parseInt(params.gid, 10);
+          }
+
+          return params;
+      };
+
+      var openPhotoSwipe = function(index, galleryElement, disableAnimation, fromURL) {
+          var pswpElement = document.querySelectorAll('.pswp')[0],
+              gallery,
+              options,
+              items;
+
+          items = parseThumbnailElements(galleryElement);
+
+          // define options (if needed)
+          options = {
+
+              // define gallery index (for URL)
+              galleryUID: galleryElement.getAttribute('data-pswp-uid'),
+
+              getThumbBoundsFn: function(index) {
+                  // See Options -> getThumbBoundsFn section of documentation for more info
+                  var thumbnail = items[index].el.getElementsByTagName('img')[0], // find thumbnail
+                      pageYScroll = window.pageYOffset || document.documentElement.scrollTop,
+                      rect = thumbnail.getBoundingClientRect(); 
+
+                  return {x:rect.left, y:rect.top + pageYScroll, w:rect.width};
+              }
+
+          };
+
+          // PhotoSwipe opened from URL
+          if(fromURL) {
+              if(options.galleryPIDs) {
+                  // parse real index when custom PIDs are used 
+                  // http://photoswipe.com/documentation/faq.html#custom-pid-in-url
+                  for(var j = 0; j < items.length; j++) {
+                      if(items[j].pid == index) {
+                          options.index = j;
+                          break;
+                      }
+                  }
+              } else {
+                  // in URL indexes start from 1
+                  options.index = parseInt(index, 10) - 1;
+              }
+          } else {
+              options.index = parseInt(index, 10);
+          }
+
+          // exit if index not found
+          if( isNaN(options.index) ) {
+              return;
+          }
+
+          if(disableAnimation) {
+              options.showAnimationDuration = 0;
+          }
+
+          // Pass data to PhotoSwipe and initialize it
+          gallery = new PhotoSwipe( pswpElement, PhotoSwipeUI_Default, items, options);
+          gallery.init();
+      };
+
+      // loop through all gallery elements and bind events
+      var galleryElements = document.querySelectorAll( gallerySelector );
+
+      for(var i = 0, l = galleryElements.length; i < l; i++) {
+          galleryElements[i].setAttribute('data-pswp-uid', i+1);
+          galleryElements[i].onclick = onThumbnailsClick;
+      }
+
+      // Parse URL and open gallery if it contains #&pid=3&gid=1
+      var hashData = photoswipeParseHash();
+      if(hashData.pid && hashData.gid) {
+          openPhotoSwipe( hashData.pid ,  galleryElements[ hashData.gid - 1 ], true, true );
+      }
+  };
+  // execute above function
+  initPhotoSwipeFromDOM('.my-gallery');
 
 });
 
--- vue/src/main.js
+++ vue/src/main.js
@@ -1,17 +1,10 @@
-// 0. If using a module system (e.g. via vue-cli), import Vue and VueRouter and then call Vue.use(VueRouter).
 
-// 1. Define route components.
-// These can be imported from other files
 const inicio = { template: '<div>-- inicio --</div>' }
 const Foo = { template: '<div><router-link to="/bar/algo">itemAlgo</router-link></div>' }
 const Bar = { template: '<div>bar</div>' }
 const barAlgo = { template: '<div>Este es barAlgo</div>' }
 
-// 2. Define some routes
-// Each route should map to a component. The "component" can
-// either be an actual component constructor created via
-// Vue.extend(), or just a component options object.
-// We'll talk about nested routes later.
+
 const routes = [
   { path: '/', component: inicio },
   { path: '/foo', component: Foo },
@@ -19,19 +12,11 @@ const routes = [
   { path: '/bar/algo', component: barAlgo }
 ]
 
-// 3. Create the router instance and pass the `routes` option
-// You can pass in additional options here, but let's
-// keep it simple for now.
 const router = new VueRouter({
-  mode: 'history',
+  //mode: 'history',
   routes // short for routes: routes
 })
 
-// 4. Create and mount the root instance.
-// Make sure to inject the router with the router option to make the
-// whole app router-aware.
 const app = new Vue({
   router
 }).$mount('#app')
-
-// Now the app has started!
